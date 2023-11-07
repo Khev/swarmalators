22961da6cb78c2e25c33c3dcb1fdeeab9e20d824
kev <kevin.p.okeeffee@gmail.com>
2023-11-06 19:20:09 -0800

    torus,new files

---
+++ 2D/on-torus/run_tasks_2d_torus.py
@@ -0,0 +1,50 @@
+""" Next: increase sampling near mixed region. Zoom in on it.
+
+    Vary:
+        - dt
+        - T
+        - N 
+        - N_trials
+
+ Do we see covergence with this?
+"""
+
+import os
+import logging
+import time
+from utils import header_print
+
+# Set up basic logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+logger = logging.getLogger()
+
+# Args
+num_workers = 9
+kmin = -3.0
+kmax = 7.0
+numk = 101
+T = 200
+
+dist_type = 'cauchy_random'
+for n_trial in [1]:
+    for n in [10**5]:
+        header = f'Starting simulation with N = {n} and distribution type = {dist_type}'
+        header_print(header) 
+        start_time = time.time()  # Get the current time in seconds since the Epoch
+        cmd = (
+                f'python compute_S_versus_K_2d_torus.py '
+                f'--n={n} '
+                f'--T={T} '
+                f'--kmin={kmin} '
+                f'--kmax={kmax} '
+                f'--numk={numk} '
+                f'--omega_dist={dist_type} '
+                f'--n_trials={n_trial} '
+                f'--num_workers={num_workers} '
+                f'--parallel'
+            )
+        os.system(cmd)
+        end_time = time.time()  # Get the current time in seconds since the Epoch
+        elapsed_time = end_time - start_time
+        logger.info(f"Finished simulation for N = {n}, Distribution = {dist_type}")
+        logger.info(f"Elapsed time: {time.strftime('%H:%M:%S', time.gmtime(elapsed_time))}")
